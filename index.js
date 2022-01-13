b3f2d7d474ff37dda404a3b6ad13dd970447e6ca
meeonn <meeonn@meeonn.com>
2022-01-13 08:55:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! - MEEONN - AUTOMATION - TESTE 2");
+    response.end("Hello World! - MEEONN - AUTOMATION - TESTE 2 - Magnus Boiola");
 });
 
 const port = process.env.PORT || 1337;
